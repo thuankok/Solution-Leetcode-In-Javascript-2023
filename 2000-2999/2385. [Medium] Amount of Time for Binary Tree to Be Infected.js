10453f392001f4dd2f2e952c51b510da2d39e2f4
Nguyen Thuan <87663023+thuankxk2701@users.noreply.github.com>
2024-01-10 02:35:36 +0000

    Solution: 2385. [Medium] Amount of Time for Binary Tree to Be Infected.js

---
+++ 2000-2999/2385. [Medium] Amount of Time for Binary Tree to Be Infected.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/description
+
+var amountOfTime = function(root, start) {
+ let amount = 0;
+    const traverse = (root, start) => {
+        if (!root) {
+            return 0;
+        }
+
+        let left = traverse(root.left, start);
+        let right = traverse(root.right, start);
+
+        if (root.val === start) {
+            amount = Math.max(left, right);
+            return -1;
+        } else if (left >= 0 && right >= 0) {
+            return Math.max(left, right) + 1;
+        } else {
+            amount = Math.max(amount, Math.abs(left - right));
+            return Math.min(left, right) - 1;
+        }
+    }
+    traverse(root, start)
+    
+    return amount;
+};
