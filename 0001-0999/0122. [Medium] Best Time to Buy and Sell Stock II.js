ab62aa440041ab6473d6125bf428c4511b4f58b9
Nguyen Thuan <87663023+thuankxk2701@users.noreply.github.com>
2024-01-10 09:36:20 +0000

    Solution: 0122. [Medium] Best Time to Buy and Sell Stock II.js

---
+++ 0001-0999/0122. [Medium] Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description
+
+var maxProfit = function(prices) {
+    let maxProfit = 0
+    for(let i=1; i< prices.length ; i++){
+        if(prices[i]>prices[i-1]){
+            maxProfit +=prices[i]-prices[i-1];
+        }
+    }
+
+    return maxProfit;
+};
