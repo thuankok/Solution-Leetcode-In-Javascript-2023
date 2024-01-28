13ce85a25fd5709d5cdf883853d3516548891a9d
Nguyen Thuan <87663023+thuankxk2701@users.noreply.github.com>
2024-01-28 14:53:31 +0000

    Solution: 1074. [Hard] Number of Submatrices That Sum to Target.js

---
+++ 1000-1999/1074. [Hard] Number of Submatrices That Sum to Target.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/description
+
+var numSubmatrixSumTarget = function(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    for (let row = 0; row < m; row++) {
+        for (let col = 1; col < n; col++) {
+            matrix[row][col] += matrix[row][col - 1];
+        }
+    }
+    let result=0;
+    for (let c1 = 0; c1 < n; c1++) {
+        for (let c2 = c1; c2 < n; c2++) {
+            const map = new Map();
+            map.set(0, 1);
+            let sum = 0;
+
+            for (let row = 0; row < m; row++) {
+                sum += matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0);
+                result += map.get(sum - target) || 0;
+                map.set(sum, (map.get(sum) || 0) + 1);
+            }
+        }
+    }
+
+    return result;
+
+
+};
