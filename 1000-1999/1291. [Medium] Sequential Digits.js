645509be7fffda7c222f3f41ce1ecc4232bc0e34
Nguyen Thuan <87663023+thuankxk2701@users.noreply.github.com>
2024-02-03 13:47:20 +0000

    Solution: 1291. [Medium] Sequential Digits.js

---
+++ 1000-1999/1291. [Medium] Sequential Digits.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/sequential-digits/description
+
+var sequentialDigits = function(low, high) {
+     const a = [];
+
+    for (let i = 1; i <= 9; ++i) {
+        let num = i;
+        let nextDigit = i + 1;
+
+        while (num <= high && nextDigit <= 9) {
+            num = num * 10 + nextDigit;
+            if (low <= num && num <= high) {
+                    a.push(num);
+            }
+            ++nextDigit;
+            }
+        }
+
+    a.sort((a, b) => a - b);
+    return a;
+};
